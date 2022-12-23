813084c8168f6e2559f3b6a82f4850c2d6459216
phuc <huyphuc0906@gmail.com>
2022-12-23 11:37:13 +0700

    Apple and Orange

---
+++ src/main/kotlin/algorithms/AppleAndOrange/AppleAndOrange.kt
@@ -0,0 +1,56 @@
+/*
+ * Complete the 'countApplesAndOranges' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER s
+ *  2. INTEGER t
+ *  3. INTEGER a
+ *  4. INTEGER b
+ *  5. INTEGER_ARRAY apples
+ *  6. INTEGER_ARRAY oranges
+ */
+
+fun countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: Array<Int>, oranges: Array<Int>): Unit {
+    var appleCount = 0
+    var orangeCount = 0
+    for (apple in apples) {
+        if (a + apple in s .. t) {
+            appleCount++
+        }
+    }
+
+    for (orange in oranges) {
+        if (b + orange in s .. t) {
+            orangeCount++
+        }
+    }
+
+    println(appleCount)
+    println(orangeCount)
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val s = first_multiple_input[0].toInt()
+
+    val t = first_multiple_input[1].toInt()
+
+    val second_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val a = second_multiple_input[0].toInt()
+
+    val b = second_multiple_input[1].toInt()
+
+    val third_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val m = third_multiple_input[0].toInt()
+
+    val n = third_multiple_input[1].toInt()
+
+    val apples = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val oranges = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
+}
