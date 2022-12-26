b3eecb2573796b3b3e69abd346ef20654ef9d3a3
phuc <huyphuc0906@gmail.com>
2022-12-26 15:15:51 +0700

    Divisible Sum Pairs

---
+++ src/main/kotlin/algorithms/DivisibleSumPairs/DivisibleSumPairs.kt
@@ -0,0 +1,37 @@
+/*
+ * Complete the 'divisibleSumPairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
+
+fun divisibleSumPairs(n: Int, k: Int, ar: Array<Int>): Int {
+    var count = 0
+
+    for (i in 0 until n - 1) {
+        for (j in i + 1 until n) {
+            if ((ar[i] + ar[j]) % k == 0) {
+                count++
+            }
+        }
+    }
+
+    return count
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val k = first_multiple_input[1].toInt()
+
+    val ar = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+
+    val result = divisibleSumPairs(n, k, ar)
+
+    println(result)
+}
