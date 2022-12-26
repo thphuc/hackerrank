4d62186041f10c8696ebb6697cca5d17c86b2582
phuc <huyphuc0906@gmail.com>
2022-12-26 16:04:47 +0700

    Migratory Birds

---
+++ src/main/kotlin/algorithms/MigratoryBirds/MigratoryBirds.kt
@@ -0,0 +1,30 @@
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fun migratoryBirds(arr: Array<Int>): Int {
+    val map = mutableMapOf<Int, Int>()
+    map[1] = 0
+    map[2] = 0
+    map[3] = 0
+    map[4] = 0
+    map[5] = 0
+    for (i in arr) {
+        map[i] = map.getValue(i) + 1
+    }
+
+    return map.maxBy { it.value }.key
+}
+
+fun main(args: Array<String>) {
+    val arrCount = readLine()!!.trim().toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+
+    val result = migratoryBirds(arr)
+
+    println(result)
+}
