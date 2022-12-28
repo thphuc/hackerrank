97a8cbda1d7f16545b83758e5672c3c179892440
phuc <huyphuc0906@gmail.com>
2022-12-28 16:14:02 +0700

    Hurdle Race

---
+++ src/main/kotlin/algorithms/HurdleRace/HurdleRace.kt
@@ -0,0 +1,33 @@
+import kotlin.math.ceil
+
+/*
+ * Complete the 'hurdleRace' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY height
+ */
+
+fun hurdleRace(k: Int, height: Array<Int>): Int {
+    val max = height.max()
+
+    if (k >= max!!) {
+        return 0
+    }
+    return max!! - k
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val k = first_multiple_input[1].toInt()
+
+    val height = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = hurdleRace(k, height)
+
+    println(result)
+}
