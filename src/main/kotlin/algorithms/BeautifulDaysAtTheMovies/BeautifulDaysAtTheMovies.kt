79d287444332e742df5a9e0fb97a93145f198038
phuc <huyphuc0906@gmail.com>
2022-12-30 08:23:19 +0700

    Beautiful Days at the movies

---
+++ src/main/kotlin/algorithms/BeautifulDaysAtTheMovies/BeautifulDaysAtTheMovies.kt
@@ -0,0 +1,36 @@
+/*
+ * Complete the 'beautifulDays' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER i
+ *  2. INTEGER j
+ *  3. INTEGER k
+ */
+
+fun beautifulDays(i: Int, j: Int, k: Int): Int {
+    var beautifulDayCount = 0
+
+    for (day in i .. j) {
+        val reversedDay = day.toString().reversed().toInt()
+
+        if (kotlin.math.abs(day - reversedDay) % k == 0) {
+            beautifulDayCount++
+        }
+    }
+    return beautifulDayCount
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val i = first_multiple_input[0].toInt()
+
+    val j = first_multiple_input[1].toInt()
+
+    val k = first_multiple_input[2].toInt()
+
+    val result = beautifulDays(i, j, k)
+
+    println(result)
+}
